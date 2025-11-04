414c51a27cc8c004a43b3bd2eb78f7dfa6239562
Milan Klöwer <milankloewer@gmx.de>
2025-11-04 15:18:09 +0000

    Create prime_meridian.jl with party information
    Add prime_meridian.jl with party details and departures.

---
+++ submissions/prime_meridian.jl
@@ -0,0 +1,8 @@
+name = "Sir Francis Drake"
+description = "Prime Meridian Party"
+
+nchildren = 26
+layer = 8
+
+# like list comprehension in Python
+departures = [(0, lat) for lat in range(-90, 90, length=26)]
