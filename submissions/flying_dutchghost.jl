964353cd86a296abbf5586b3e10856771cc6afe6
Milan Klöwer <milankloewer@gmx.de>
2025-11-04 01:15:19 +0000

    Add comments to departure coordinates
    Updated departure coordinates with comments for clarity.

--- submissions/flying_dutchghost.jl
+++ submissions/flying_dutchghost.jl
@@ -1,11 +1,12 @@
 author = "Flying Dutchman and the Ghost"
 description = "Five on the equator"
+
 nchildren = 5
 layer = 8
 departures = [
-    (5, 0),
-    (-5, 0),
-    (-15, 0),
+    (  5, 0),    # (lon, lat) in degrees for particle 1
+    ( -5, 0),    # particle 2
+    (-15, 0),    # etc
     (-25, 0),
     (-35, 0),
 ]
