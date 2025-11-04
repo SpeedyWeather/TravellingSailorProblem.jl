5d78b13be4fde0592a730e7d24f2e89c7042d80b
Milan Klöwer <milankloewer@gmx.de>
2025-11-04 02:06:58 +0000

    Update flying_dutchghost.jl

--- submissions/flying_dutchghost.jl
+++ submissions/flying_dutchghost.jl
@@ -1,12 +1,12 @@
-author = "Flying Dutchman and the Ghost"
-description = "Five on the equator"
+name = "Flying Dutchman and the Ghost"
+description = "Five on the Interstate 10"
 
 nchildren = 5
 layer = 8
 departures = [
-    (  5, 0),    # (lon, lat) in degrees for particle 1
-    ( -5, 0),    # particle 2
-    (-15, 0),    # etc
-    (-25, 0),
-    (-35, 0),
+    ( -82, 30),    # (lon, lat) in degrees for particle 1
+    ( -90, 31),    # particle 2
+    ( -98, 32),    # etc
+    (-106, 33),
+    (-114, 34),
 ]
