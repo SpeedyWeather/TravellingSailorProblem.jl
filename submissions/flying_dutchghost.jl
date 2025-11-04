8346117ce805696c1f156a1c7154a16ea6a8790f
Milan Klöwer <milankloewer@gmx.de>
2025-11-04 01:12:00 +0000

    Add flying_dutchghost.jl submission

---
+++ submissions/flying_dutchghost.jl
@@ -0,0 +1,11 @@
+author = "Flying Dutchman and the Ghost"
+description = "Five on the equator"
+nchildren = 5
+layer = 8
+departures = [
+    (5, 0),
+    (-5, 0),
+    (-15, 0),
+    (-25, 0),
+    (-35, 0),
+]
