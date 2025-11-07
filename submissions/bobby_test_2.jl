4deb42af086d167ced9acf9bdee59ca12b90f656
Milan Klöwer <milankloewer@gmx.de>
2025-11-07 12:44:09 +0000

    delete old

--- submissions/bobby_test_2.jl
+++
@@ -1,8 +0,0 @@
-name = "Spongebob Squarepants"
-description = "1 child"
-
-nchildren = 1      # [1, 26]
-layer = 5           # [1, 8], 1 is top layer, 8 is surface layer
-
-# 26 random locations (lon, lat) in degrees ˚E, ˚N
-departures = [(15.35,  29.01)]
