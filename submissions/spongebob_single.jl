7ba97b3137b9ae554f66192721d4657a71bd4633
Milan Klöwer <milankloewer@gmx.de>
2025-11-07 12:43:59 +0000

    Hopefully hit Ana in Winnipeg?

---
+++ submissions/spongebob_single.jl
@@ -0,0 +1,8 @@
+name = "Spongebob Squarepants"
+description = "1 child"
+
+nchildren = 1      # [1, 26]
+layer = 8          # [1, 8], 1 is top layer, 8 is surface layer
+
+# single locations (lon, lat) in degrees ˚E, ˚N
+departures = [(0, 61.2)]
