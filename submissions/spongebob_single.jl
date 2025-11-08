e591a871927b75ea5582b0e4891212068fd5278d
Milan Klöwer <milankloewer@gmx.de>
2025-11-08 12:11:02 +0000

    Update departure latitude in spongebob_single.jl

--- submissions/spongebob_single.jl
+++ submissions/spongebob_single.jl
@@ -5,4 +5,4 @@ nchildren = 1      # [1, 26]
 layer = 8          # [1, 8], 1 is top layer, 8 is surface layer
 
 # single locations (lon, lat) in degrees ˚E, ˚N
-departures = [(0, 61.2)]
+departures = [(0, 61.3)]
